1c306f21c7d7219604fff551429c76fbb7a8d09f
xd23fe39 <xd23fe39@yahoo.de>
2019-07-28 10:09:43 +0200

    Aktualisiert

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,35 @@
-#include <Arduino.h>
+/*
+  URL: https://github.com/xd23fe39/ESP32-AnalogDigitalTest
 
-long treffer = 0;
+  Erfolgreich getestet auf:
+  - Espressif ESP32 WLAN Dev Kit Board Development Bluetooth Wifi v1 WROOM32 NodeMCU
+
+  PIN Layout:
+
+  ESP32        | Poti
+  -------------|----------
+  3V3          | VCC
+  GND          | GND
+  VP (GPIO36)  | SIG
+
+*/
+#include "Arduino.h"
+
+unsigned long counter = 0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.println("\nESP32 NodeMCU-32\n");
   // initialisiere ADC Interface
-  analogSetWidth(12);
-  analogSetAttenuation(ADC_11db);
+  analogSetWidth(10);                   // mögliche Werte: 10-12 
+  analogSetAttenuation(ADC_11db);       // bei Poti an 3.3V Spannung 
 }
 
 void loop() {
   int analog_in;
-  analog_in = analogRead(36);
-  Serial.print(++treffer);
+  analog_in = analogRead(36);           // lese analogen Wert an ADC0 (GPIO36, VP)
+  Serial.print(++counter);
   Serial.print(" ");
   Serial.print(analog_in);
   Serial.println();
